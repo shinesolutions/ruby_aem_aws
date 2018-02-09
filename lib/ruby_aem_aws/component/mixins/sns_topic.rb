231b2eddf205f4a562220887aca298f57aacb896
Michael Bloch <michael.bloch@shinesolutions.com>
2018-02-09 17:10:17 +1100

    Fixed syntax error of rubocop

--- lib/ruby_aem_aws/component/mixins/sns_topic.rb
+++ lib/ruby_aem_aws/component/mixins/sns_topic.rb
@@ -1,18 +1,18 @@
 require 'aws-sdk-sns'
 
 module RubyAemAws
-	module SNSTopic
-		# @param topicarn the ARN of the SNS Topix to publish the message
+  # AWS Interface publish to SNS-Topic
+  module SNSTopic
+    # @param topicarn the ARN of the SNS Topix to publish the message
     # @param sns_message the SNS Message to publish
-		# @return Message ID
-		def publish(topicarn, sns_message)
-			client = Aws::SNS::Topic.new(topicarn)
-			publish = client.publish({
-                                subject: "Publish",
-                                message: sns_message,
-                                message_structure: "json",
-                        })
-			publish.message_id
-		end
-	end
+    # @return Message ID
+    def publish(topicarn, sns_message)
+      client = Aws::SNS::Topic.new(topicarn)
+      sns_message = { subject: 'Publish',
+                      message: sns_message,
+                      message_structure: 'json' }
+      publish = client.publish(sns_message)
+      publish.message_id
+    end
+  end
 end
--- lib/ruby_aem_aws/component/stack_manager.rb
+++ lib/ruby_aem_aws/component/stack_manager.rb
@@ -2,35 +2,37 @@ require_relative 'mixins/sns_topic'
 require_relative 'mixins/dynamo_db'
 
 module RubyAemAws
-	module Component
-		class StackManagerTest
-			include DynamoDB
-			include SNSTopic
+  module Component
+    # Interface to the AWS StackManager to send out commands
+    class StackManagerTest
+      include DynamoDB
+      include SNSTopic
 
-			def initialize(topicarn, stack_prefix)
-				@topicarn = topicarn
-				@stack_prefix = stack_prefix
-				@retry_opts = {:max_tries => 5,
-					:base_sleep_seconds => 3.0,
-					:max_sleep_seconds => 15.5
-				}
-			end
-			# @param task AWS SSM run_cmd
-			# @param details SNS Message details
-			# @return Command state
-			def check(task, details)
-				sns_message = "{ \"default\": \"{ 'task': '#{task}', 'stack_prefix': '#{@stack_prefix}', 'details': { #{details} }}\"}"
-				message_id = publish(@topicarn, sns_message)
+      def initialize(topicarn, stack_prefix)
+        @topicarn = topicarn
+        @stack_prefix = stack_prefix
+        @retry_opts = { max_tries: 5,
+                        base_sleep_seconds: 5.0,
+                        max_sleep_seconds: 15.5 }
+        super("#{@stack_prefix}-AemStackManagerTable", 'command_id', 'EQ')
+      end
+      # @param task AWS SSM run_cmd
+      # @param details SNS Message details
+      # @return Command state
 
-				with_retries(@retry_opts) do
-					@cmd_id = scan("#{@stack_prefix}-AemStackManagerTable", 'command_id', 'message_id', message_id, 'EQ').items[0]['command_id']
-		  	end
+      def check(task, parameters)
+        details = JSON.generate(parameters).tr('\"', '\'')
+        sns_message = "{ \"default\": \"{ 'task': '#{task}', 'stack_prefix': '#{@stack_prefix}', 'details': #{details} }\"}"
+        message_id = publish(@topicarn, sns_message)
 
-				with_retries(@retry_opts) do
-					state = query("#{@stack_prefix}-AemStackManagerTable", 'state', 'command_id', @cmd_id, 'EQ', 'state', 'Pending', 'NE').items[0]['state']
-					puts "Task execution: #{state}"
-				end
-			end
-		end
-	end
+        with_retries(@retry_opts) do
+          @cmd_id = scan('command_id', 'message_id', message_id, 'EQ').items[0]['command_id']
+        end
+
+        with_retries(@retry_opts) do
+          query('state', @cmd_id, 'state', 'Pending', 'NE').items[0]['state']
+        end
+      end
+    end
+  end
 end
